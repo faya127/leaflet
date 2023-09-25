6de4b6407bbad803df909d69fe4aa1d6b3ff711a
faya127 <faya.husna0702@mail.ugm.ac.id>
2023-09-25 10:44:03 +0700

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  map!: L.Map;
+  baseMaps:
+    | {
+        Street: L.TileLayer;
+        Kontur: L.TileLayer;
+        Topo: L.TileLayer;
+      }
+    | undefined;
+  constructor() {}
+  //ngOnInit() {
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([35.76943, -5.80081], 12);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.map);
+
+    this.baseMaps = {
+      Street: L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }
+      ),
+      Kontur: L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://opentopomap.org/about" target="_blank">OpenTopoMap</a> contributors',
+      }),
+      Topo: L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+        attribution:
+          '&copy; <a href="https://maps.google.com/">Google Maps</a>',
+      }),
+    };
+
+    this.baseMaps['Street'].addTo(this.map);
+
+    L.control.layers(this.baseMaps).addTo(this.map);
+
+    // Menambahkan marker
+    const marker = new L.Marker([35.76943, -5.80081]);
+
+    // Menambahkan marker ke map
+    this.map.addLayer(marker);
+
+    // Menambahkan Pop up
+    marker.bindPopup('Coba Pop up ayooooo semangat');
+  }
+}
